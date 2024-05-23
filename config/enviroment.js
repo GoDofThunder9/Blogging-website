ad7c7f5cbf1c19720e315e27214d9fe1fabc7fc9
GoDofThunder9 <123013319+GoDofThunder9@users.noreply.github.com>
2024-05-24 01:37:53 +0530

    updated enviroment removed the rotateing file stream

--- config/enviroment.js
+++ config/enviroment.js
@@ -3,10 +3,6 @@
         assets: 'assets',
         session_cookie_key:'3XWyqFymkFeptIdb0l0JtLafQ9udB5sm',
         db: 'name',
-        morgan:{
-            mode:'dev',
-            options:{stream: accessLogStream}
-        }
         
     }
 
@@ -15,9 +11,5 @@
         assets: process.env.assets,
         session_cookie_key:process.env.session_cookie_key,
         db: process.env.db,
-        morgan:{
-            mode:'combined',
-            options:{stream: accessLogStream}
-        }
     }
     module.exports = eval(process.env.CODEIAL_ENVIROMENT)== undefined ? development : eval(process.env.CODEIAL_ENVIROMENT);
